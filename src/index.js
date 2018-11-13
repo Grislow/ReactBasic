5a36e3922c0b8bc6765081159d3078c9d8606861
Grzegorz Maj <gmaj@inbrackets.pl>
2018-11-13 17:26:48 +0100

    Introduction to JSX

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
+// Import the React and ReactDOM
+//  -using ES6 modules
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// defining a function to be used within the functional component
+function getButtonText() {
+    // return { text: 'Click me'}   -> cannot parse js objects as text implicitly
+    return 'Click me!';
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Create a react component
+//  -this is a functional component
+const App = () => {
+
+    const labelText = 'Enter name:';
+
+    return (
+    <div>
+        {/* 'for' property needed to be renamed to 'htmlFor', check in console to register these types of errors */}
+        <label  htmlFor="name" className="label">{labelText}</label>
+        <input type="text" id="name"/>
+        {/* 
+            -Styles must be passed as an object
+            -replace normal css properties with jsx compatible ones by removing dashes and using camelCase notation
+         */}
+        <button style={{color: 'white', backgroundColor: 'blue'}}>
+        {/* To reference js variables and function wrap code in curly braces */}
+            {getButtonText()}
+        </button>
+    </div>
+    );
+};
+
+// Take the react component and show it on the screen
+ReactDOM.render(
+    // Earlier created react components
+    <App />,
+    // Dom element within which this component will be rendered
+    document.querySelector('#root')
+);
