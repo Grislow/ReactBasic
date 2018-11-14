359d72a6d9340dd29d81cf2ec0649c2aba44e406
Grzegorz Maj <gmaj@inbrackets.pl>
2018-11-14 15:31:50 +0100

    Introduction to State in React

--- src/index.js
+++ src/index.js
@@ -1,52 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
 
-import CommentDetail from './CommentDetail';
-import ApprovalCard from './ApprovalCard';
+import SeasonDisplay from './SeasonDisplay';
+import Loader from './Loader';
 
+/*
+    1. Is a js class
+    2. extends React.Component
+        -comes with a ton of built in functionality
+    3. has render() method that returns jsx
+*/
+class App extends React.Component {
+    constructor(props) {
+        //ensures that the React.Component constructor is called
+        super(props);
+        
+        // 1 approach of state initialization - in constructor
+        // initialization is the only situation where we assign value directly
+        this.state = {
+            // make sure to default state value(null for numerical, empty string for string)
+            lat: null,
+            errorMessage: ''
+        };
+    }
 
-const App = () => {
-    return (
-        <div className="ui container comments">
-            <ApprovalCard>
-                <div>
-                    <h4>Pretty Cool</h4>
-                    This will work. If div omitted it wouldnt work
-                </div>
-            </ApprovalCard>
-            <ApprovalCard>
-                {/* Nested component available as props.children in parent component */}
-                <CommentDetail 
-                    author="Sam" 
-                    timeAgo="Today at 4:45PM"
-                    comment="React is pretty cool"
-                    avatar={faker.image.avatar()}
-                />
-            </ApprovalCard>
-            <ApprovalCard>
-                {/* 
-                    -Nesting CommentDetail component
-                    -passing author/timeAgo/comment prop
-                */}
-                <CommentDetail 
-                    author="George" 
-                    timeAgo="Yesterday at 3:12AM"
-                    comment="Yes, finally i can have a nice modular codebase for my front end"
-                    avatar={faker.image.avatar()}
-                />
-            </ApprovalCard>
-            <ApprovalCard>
-                <CommentDetail 
-                    author="Jenna" 
-                    timeAgo="Feb 12 3:00AM"
-                    comment="I prefer the very popular vanilla js lib"
-                    avatar={faker.image.avatar()}
-                />
-            </ApprovalCard>
-        </div>
-    );
-};
+    //2 approach of state initialization - in class body
+    // SAME AS INITIALIZING IN CONSTRUCTOR BUT ALLOWS OMISSION OF CONSTRUCTOR
+    // state = { lat: null, errorMessage: ''};
+
+    // example of lifecycle method
+    // componentDidMount() - is called when component gets rendered to DOM for the first time
+    componentDidMount() {
+        console.log('Component rendered to screen');
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                // example of setting the state
+                // setState is inherited from React.Component     
+                this.setState({ lat: position.coords.latitude});
+            },
+            err =>  {
+                // this way whenever an error occurs the component is rerendered
+                this.setState({ errorMessage: err.message });
+            }
+        );
+    }
+    // componentDidUpdate() - is called after component is updated and rerendered
+    componentDidUpdate() {
+        console.log('Component was updated');
+    }
+
+    // Helper functions isolates conditional rendering
+    // Thx to this the render function only includes what common across all situations
+    renderContent() {
+        // example of conditional rendering
+        if (this.state.errorMessage && !this.state.lat){
+            return <div>Error: {this.state.errorMessage} </div>;
+        } else if(this.state.lat && !this.state.errorMessage) {
+            return <SeasonDisplay lat={this.state.lat}/>;
+        } else {
+            return <Loader text="Please accept location request"/>;
+        }
+    }
+
+    // minimize ammount of logic in the render method - it gets called a lot, should be succinct
+    render() {
+        return <div>{this.renderContent()}</div>;
+    }
+}
 
 ReactDOM.render(
     <App />,
