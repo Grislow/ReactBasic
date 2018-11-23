50098feb49ebf67c901d94ff1cc1fc369e1f1b4c
Grzegorz Maj <gmaj@inbrackets.pl>
2018-11-23 18:22:15 +0100

    Redux in depth

---
+++ src/actions/types.js
@@ -0,0 +1,3 @@
+export const FETCH_POSTS = 'fetch_posts';
+
+export const FETCH_USER = 'fetch_user';
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,23 +1,8 @@
 import { combineReducers } from 'redux';
-
-const songsReducer = () => {
-    return [
-        { title: 'Gangsta\'s paradise' , duration: '4:17' },
-        { title: 'Walk it Talk it' , duration: '4:38' },
-        { title: 'T-shirt' , duration: '4:53' },
-        { title: 'Motorsport' , duration: '5:18' }
-    ];
-};
-
-const selectedSongReducer = (selectedSong=null, action) => {
-    if( action.type === 'SONG_SELECTED' ) {
-        return action.payload;
-    }
-
-    return selectedSong;
-};
+import postsReducer from './postsReducer';
+import usersReducer from './usersReducer';
 
 export default combineReducers({
-    songs: songsReducer,
-    selectedSong: selectedSongReducer
+    posts: postsReducer,
+    users: usersReducer
 });
